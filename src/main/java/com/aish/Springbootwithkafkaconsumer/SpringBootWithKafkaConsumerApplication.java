ae6a7dd8c4a65bbd2781e5e7d0c2eb616272e7eb
AishwaryaJorve <aishwaryajorve1604@gmail.com>
2021-04-25 10:38:05 +0530

    initial commit

---
+++ src/main/java/com/aish/Springbootwithkafkaconsumer/SpringBootWithKafkaConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.aish.Springbootwithkafkaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithKafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithKafkaConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aish/Springbootwithkafkaconsumer/SpringBootWithKafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aish.Springbootwithkafkaconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithKafkaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
